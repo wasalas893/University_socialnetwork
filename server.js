be079f1586bb84943970f1acea03c4a0578b8f45
wasalas <wasalas893@gmail.com>
2020-05-07 10:40:11 +0530

    create the backend

---
+++ server.js
@@ -0,0 +1,17 @@
+const express=require('express');
+const connectDB=require('./config/db');
+
+
+
+
+const app=express();
+
+//Connect Database
+connectDB();
+
+//
+
+app.get('/',(req,res)=>res.send('API Running...'));
+
+const PORT=process.env.PORT || 5000
+app.listen(PORT,()=>console.log(`server started on port ${PORT}`));
